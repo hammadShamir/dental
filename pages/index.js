16ae9f91da5074b0aa0cbfc6f0bbabd3e7fb9a2f
hammadShamir <hammadShamir7@gmail.com>
2023-05-05 03:36:08 +0500

    Initial Setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavbarFour from "../components/_App/NavbarFour"
+const index = () => {
+  return (
+    <>
+    <NavbarFour/>
+    </>
+  )
+}
+
+export default index
