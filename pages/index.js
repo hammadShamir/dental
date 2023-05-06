bdb58ad8d62bd63a04a15aea0fce1a6a9bffbc7e
hammadShamir <hammadShamir7@gmail.com>
2023-05-06 13:21:55 +0500

    Added Slider

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavbarFour from "../components/_App/NavbarFour"
+import MainBanner from "../components/HomeEight/MainBanner"
 const index = () => {
   return (
     <>
-    <NavbarFour/>
+      <NavbarFour />
+      <MainBanner />
     </>
   )
 }
