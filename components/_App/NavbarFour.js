4252ed848b0a32105589a4ecd766b9d6b4498c7a
AdeelAtta <adeelatta@gmail.com>
2023-05-06 16:05:43 +0500

    menubar design improved

--- components/_App/NavbarFour.js
+++ components/_App/NavbarFour.js
@@ -49,7 +49,7 @@ const NavbarFour = () => {
               <nav className="navbar navbar-expand-md navbar-light">
                 <div className="container">
                   <Link href="/" className="navbar-brand">
-                    Logo
+                    Esthentix Dental Center
                   </Link>
 
                   <button
@@ -69,6 +69,7 @@ const NavbarFour = () => {
 
                   <div className={classOne} id="navbarSupportedContent">
                     <ul className="navbar-nav m-auto">
+                      {/* Home */}
                       <li className="nav-item">
                         <Link
                           href="#"
@@ -76,248 +77,54 @@ const NavbarFour = () => {
                           className="nav-link"
                         >
                           Home
-                          {/* <i className="bx bx-plus"></i> */}
                         </Link>
-
-                        {/* <ul className="dropdown-menu">
-                          <li className="nav-item">
-                            <Link
-                              href="/"
-                              className={`nav-link ${currentPath == "/" && "active"
-                                }`}
-                            >
-                              Home 1 (Emergency Medical Clinic)
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/index-2/"
-                              className={`nav-link ${currentPath == "/index-2/" && "active"
-                                }`}
-                            >
-                              Home 2 (Covid-19 Treatment Clinic)
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/index-3/"
-                              className={`nav-link ${currentPath == "/index-3/" && "active"
-                                }`}
-                            >
-                              Home 3 (Covid-19 Test Center)
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/index-4/"
-                              className={`nav-link ${currentPath == "/index-4/" && "active"
-                                }`}
-                            >
-                              Home 4 (Vaccination Center/Clinic)
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/index-5/"
-                              className={`nav-link ${currentPath == "/index-5/" && "active"
-                                }`}
-                            >
-                              Home 5 (Doctors Directory Listing)
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/index-6/"
-                              className={`nav-link ${currentPath == "/index-6/" && "active"
-                                }`}
-                            >
-                              Home 6 (Health Charity Event)
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/index-7/"
-                              className={`nav-link ${currentPath == "/index-7/" && "active"
-                                }`}
-                            >
-                              Home 7 (Medical Research)
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/index-8/"
-                              className={`nav-link ${currentPath == "/index-8/" && "active"
-                                }`}
-                            >
-                              Home 8 (Dental Clinic)
-                            </Link>
-                          </li>
-                        </ul> */}
                       </li>
 
+                      {/* About */}
                       <li className="nav-item">
                         <Link
                           href="/about/"
                           className={`nav-link ${currentPath == "/about/" && "active"
                             }`}
                         >
-                          About
+                          About us
+                           <i className="bx bx-plus"></i>
                         </Link>
+                        <ul className="dropdown-menu">
+                          <li className="nav-item">
+                            <Link
+                              href="/mission"
+                              className={`nav-link ${currentPath == "/mission" && "active"
+                                }`}
+                            >
+                              Our Mission
+                            </Link>
+                          </li>
+
+                          <li className="nav-item">
+                            <Link
+                              href="/vision"
+                              className={`nav-link ${currentPath == "/vision" && "active"
+                                }`}
+                            >
+                              Our Vision
+                            </Link>
+                          </li>
+
+                          <li className="nav-item">
+                            <Link
+                              href="/team"
+                              className={`nav-link ${currentPath == "/team" && "active"
+                                }`}
+                            >
+                              Our Team
+                            </Link>
+                          </li>
+                        </ul>
                       </li>
 
-                      <li className="nav-item">
-                        <Link
-                          href="#"
-                          onClick={(e) => e.preventDefault()}
-                          className="nav-link"
-                        >
-                          Pages
-                          {/* <i className="bx bx-plus"></i> */}
-                        </Link>
 
-                        {/* <ul className="dropdown-menu">
-                          <li className="nav-item">
-                            <Link
-                              href="/pricing/"
-                              className={`nav-link ${currentPath == "/pricing/" && "active"
-                                }`}
-                            >
-                              Pricing
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/testimonials/"
-                              className={`nav-link ${currentPath == "/testimonials/" && "active"
-                                }`}
-                            >
-                              Testimonials
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/doctors/"
-                              className={`nav-link ${currentPath == "/doctors/" && "active"
-                                }`}
-                            >
-                              Doctors
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/appointment/"
-                              className={`nav-link ${currentPath == "/appointment/" && "active"
-                                }`}
-                            >
-                              Appointment
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/faq/"
-                              className={`nav-link ${currentPath == "/faq/" && "active"
-                                }`}
-                            >
-                              FAQs
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="#"
-                              onClick={(e) => e.preventDefault()}
-                              className="nav-link"
-                            >
-                              User <i className="bx bx-plus"></i>
-                            </Link>
-
-                            <ul className="dropdown-menu">
-                              <li className="nav-item">
-                                <Link
-                                  href="/sign-in/"
-                                  className={`nav-link ${currentPath == "/sign-in/" && "active"
-                                    }`}
-                                >
-                                  Sign In
-                                </Link>
-                              </li>
-
-                              <li className="nav-item">
-                                <Link
-                                  href="/sign-up/"
-                                  className={`nav-link ${currentPath == "/sign-up/" && "active"
-                                    }`}
-                                >
-                                  Sign Up
-                                </Link>
-                              </li>
-
-                              <li className="nav-item">
-                                <Link
-                                  href="/recover-password/"
-                                  className={`nav-link ${currentPath == "/recover-password/" &&
-                                    "active"
-                                    }`}
-                                >
-                                  Recover Password
-                                </Link>
-                              </li>
-                            </ul>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/coming-soon/"
-                              className={`nav-link ${currentPath == "/coming-soon/" && "active"
-                                }`}
-                            >
-                              Coming Soon
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/terms-conditions/"
-                              className={`nav-link ${currentPath == "/terms-conditions/" && "active"
-                                }`}
-                            >
-                              Terms & Conditions
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/privacy-policy/"
-                              className={`nav-link ${currentPath == "/privacy-policy/" && "active"
-                                }`}
-                            >
-                              Privacy Policy
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/404"
-                              className={`nav-link ${currentPath == "/404/" && "active"
-                                }`}
-                            >
-                              404 Error Page
-                            </Link>
-                          </li>
-                        </ul> */}
-                      </li>
-
+                      {/* Services */}
                       <li className="nav-item">
                         <Link
                           href="#"
@@ -325,17 +132,17 @@ const NavbarFour = () => {
                           className="nav-link"
                         >
                           Services
-                          {/* <i className="bx bx-plus"></i> */}
+                          <i className="bx bx-plus"></i>
                         </Link>
 
-                        {/* <ul className="dropdown-menu">
+                     <ul className="dropdown-menu">
                           <li className="nav-item">
                             <Link
                               href="/services-1/"
                               className={`nav-link ${currentPath == "/services-1/" && "active"
                                 }`}
                             >
-                              Services Style One
+                              Root canval Treatment
                             </Link>
                           </li>
 
@@ -345,7 +152,7 @@ const NavbarFour = () => {
                               className={`nav-link ${currentPath == "/services-2/" && "active"
                                 }`}
                             >
-                              Services Style Two
+                              Orthodontic Braces
                             </Link>
                           </li>
 
@@ -355,75 +162,14 @@ const NavbarFour = () => {
                               className={`nav-link ${currentPath == "/services-3/" && "active"
                                 }`}
                             >
-                              Services Style Three
+                              Invisible Alligners
                             </Link>
                           </li>
 
-                          <li className="nav-item">
-                            <Link
-                              href="/service-details/"
-                              className={`nav-link ${currentPath == "/service-details/" && "active"
-                                }`}
-                            >
-                              Service Details
-                            </Link>
-                          </li>
-                        </ul> */}
+                        </ul> 
                       </li>
 
-                      <li className="nav-item">
-                        <Link
-                          href="#"
-                          onClick={(e) => e.preventDefault()}
-                          className="nav-link"
-                        >
-                          Doctors
-                          {/* <i className="bx bx-plus"></i> */}
-                        </Link>
-
-                        {/* <ul className="dropdown-menu">
-                          <li className="nav-item">
-                            <Link
-                              href="/doctors-1/"
-                              className={`nav-link ${currentPath == "/doctors-1/" && "active"
-                                }`}
-                            >
-                              Doctors Style One
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/doctors-2/"
-                              className={`nav-link ${currentPath == "/doctors-2/" && "active"
-                                }`}
-                            >
-                              Doctors Style Two
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/doctors-3/"
-                              className={`nav-link ${currentPath == "/doctors-3/" && "active"
-                                }`}
-                            >
-                              Doctors Style Three
-                            </Link>
-                          </li>
-
-                          <li className="nav-item">
-                            <Link
-                              href="/doctor-details/"
-                              className={`nav-link ${currentPath == "/doctor-details/" && "active"
-                                }`}
-                            >
-                              Doctors Details
-                            </Link>
-                          </li>
-                        </ul> */}
-                      </li>
-
+                      {/* Blogs */}
                       <li className="nav-item">
                         <Link
                           href="#"
@@ -478,25 +224,29 @@ const NavbarFour = () => {
                         </ul> */}
                       </li>
 
+                      {/* Contact us */}
                       <li className="nav-item">
                         <Link
                           href="/contact/"
                           className={`nav-link ${currentPath == "/contact/" && "active"
                             }`}
                         >
-                          Contact
+                          Contact us
                         </Link>
                       </li>
+
+
                     </ul>
                   </div>
 
                   <div className="others-option">
                     <div className="subscribe">
-                      <Link href="/contact" className="default-btn">
-                        Get A Quote
+                      <Link href="/appointment" className="default-btn">
+                        Book an Appointment
                       </Link>
                     </div>
                   </div>
+
                 </div>
               </nav>
             </div>
--- components/_App/TopHeader.js
+++ components/_App/TopHeader.js
@@ -10,18 +10,18 @@ const TopHeader = () => {
               <ul className="header-content-left">
                 <li>
                   <i className="bx bx-time"></i>
-                  Mon-Fri 9am-5pm
+                  Mon-sun 9am-10pm 
                 </li>
                 <li>
                   <a href="tel:+822456974">
                     <i className="bx bx-phone-call"></i>
-                    Call Us: +821-456-241
+                    Call Us: +971-4-5294-421
                   </a>
                 </li>
                 <li>
                   <a href="mailto:hello@info.com">
                     <i className="bx bxs-paper-plane"></i>
-                    Email: hello@info.com
+                    Email: info@esthetixdentalcenters.com
                   </a>
                 </li>
               </ul>
