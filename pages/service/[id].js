e3a3459a1795bd7fc0ad964fccbee4449841e27e
AdeelAtta <adeelatta@gmail.com>
2023-05-27 15:54:37 +0500

    services section image added

--- pages/service/[id].js
+++ pages/service/[id].js
@@ -40,11 +40,26 @@ const ServicesDetails = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-6">
-                            <div className="services-img mb-4">
-                                <img
-                                    src="/img/service-details/service-details1.jpg"
-                                    alt="Image"
-                                />
+                            <div className="services-img d-flex justify-content-center align-items-center mt-5">
+                                {/* todo */}
+                                <div className="single-work">
+                                    <img src="/img/work1.jpg" style={{filter: 'grayscale(1)'}} alt="Image" />
+
+                                    <h3 className="work-title " style={{top:'50%', bottom:'auto', left:'50%' , right:'auto',width:'fit-content', transform:`translate(-50%,-50%)`}}>
+                                        <i className="flaticon-kidney"></i>
+                                        {service.name}
+                                    </h3>
+
+                                    <div className="work-content-wrap">
+                                        <div className="work-content">
+                                            <h3>{service.name}</h3>
+                                            <p>
+                                                {(service.desc)}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* todo */}
                             </div>
                         </div>
 
@@ -67,7 +82,7 @@ const ServicesDetails = () => {
                                                         src={url + item.url}
                                                         alt="Image"
                                                         className="w-100"
-                                                        style={{ aspectRatio: '4/3.3' }}
+                                                        style={{ aspectRatio: '4/3',transform:'scale(0.7)' }}
                                                     />
                                                 </div>
                                             </SwiperSlide>
